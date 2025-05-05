84f0712fc662537c18c1dc6d283e4fc070c7a257
ManiSrujan <bagganisrujan@gmail.com>
2025-05-05 21:37:31 +0530

    api: adding temp log to postgresql client creation

--- api/src/utils/db.js
+++ api/src/utils/db.js
@@ -3,6 +3,13 @@ import pg from "pg";
 const { Client } = pg;
 
 function createClient() {
+  console.log("Creating a new PostgreSQL client...", {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+  });
   return new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
