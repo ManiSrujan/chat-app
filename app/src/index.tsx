20798b05f82fa13773d9894549e1a3a5a1ca9f54
ManiSrujan <bagganisrujan@gmail.com>
2025-05-24 16:03:31 +0530

    app: added sign up form ui including validation

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </React.StrictMode>,
   );
 }
